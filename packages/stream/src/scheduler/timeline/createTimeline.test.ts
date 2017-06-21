e043e8290f18fb9b1fe510c083ef267b751d1a3b
TylorS167 <tlsteinberger167@gmail.com>
2017-06-21 15:27:17 -0400

    test(stream): add more tests to createTimeline

--- packages/stream/src/scheduler/timeline/createTimeline.test.ts
+++ packages/stream/src/scheduler/timeline/createTimeline.test.ts
@@ -1,6 +1,9 @@
-import { TestCollection, describe, it } from '@typed/test'
+import { TestCollection, describe, given, it } from '@typed/test'
 
 import { Timeline } from './types'
+import { createEventTask } from '../task'
+import { createScheduledTask } from '../scheduledTask'
+import { createScheduler } from '../createScheduler'
 import { createTimeline } from './createTimeline'
 
 export const test: TestCollection = describe(`createTimeline`, [
@@ -9,4 +12,26 @@ export const test: TestCollection = describe(`createTimeline`, [
 
     ok(timeline.isEmpty())
   }),
+
+  describe(`isEmpty`, [
+    given(`The timeline is empty`, [
+      it(`returns true`, ({ ok }) => {
+        const timeline: Timeline = createTimeline()
+
+        ok(timeline.isEmpty())
+      }),
+    ]),
+
+    given(`a timeline is not empty`, [
+      it(`returns false`, ({ notOk }) => {
+        const timeline: Timeline = createTimeline()
+        const task = createEventTask(1, { event() {}, error() {}, end() {} })
+        const scheduledTask = createScheduledTask(0, 0, 0, task, createScheduler())
+
+        timeline.add(scheduledTask)
+
+        notOk(timeline.isEmpty())
+      }),
+    ]),
+  ]),
 ])

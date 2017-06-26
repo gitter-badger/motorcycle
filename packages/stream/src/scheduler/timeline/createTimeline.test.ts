d3b5f99b107a5fd117221b209dd229286a024923
TylorS167 <tlsteinberger167@gmail.com>
2017-06-26 11:08:41 -0400

    test(stream): add missing timeline tests

--- packages/stream/src/scheduler/timeline/createTimeline.test.ts
+++ packages/stream/src/scheduler/timeline/createTimeline.test.ts
@@ -1,8 +1,8 @@
+import { ScheduledTask, createScheduledTask } from '../scheduledTask'
 import { TestCollection, describe, given, it } from '@typed/test'
 
 import { Timeline } from './types'
 import { createEventTask } from '../task'
-import { createScheduledTask } from '../scheduledTask'
 import { createScheduler } from '../createScheduler'
 import { createTimeline } from './createTimeline'
 
@@ -31,6 +31,90 @@ export const test: TestCollection = describe(`createTimeline`, [
         timeline.add(scheduledTask)
 
         notOk(timeline.isEmpty())
+      }),
+    ]),
+  ]),
+
+  describe(`nextArrival`, [
+    given(`the timeline is empty`, [
+      it(`returns Infinity`, ({ equal }) => {
+        const timeline = createTimeline()
+        equal(Infinity, timeline.nextArrival())
+      }),
+    ]),
+
+    given(`the timeline is not empty`, [
+      it(`returns the time of the first scheduled task time`, ({ equal }) => {
+        const timeline: Timeline = createTimeline()
+        const task = createEventTask(1, { event() {}, error() {}, end() {} })
+        const scheduledTask = createScheduledTask(100, 0, 0, task, createScheduler())
+
+        timeline.add(scheduledTask)
+
+        equal(100, timeline.nextArrival())
+
+        // this will remove all tasks with time <= 200 from timeline
+        timeline.readyTasks(200)
+
+        const scheduledTaskTwo = createScheduledTask(1000, 0, 0, task, createScheduler())
+
+        timeline.add(scheduledTaskTwo)
+
+        equal(1000, timeline.nextArrival())
+      }),
+    ]),
+  ]),
+
+  describe(`add`, [
+    given(`a scheduled task`, [
+      it(`adds the task to the timeline`, ({ same }) => {
+        const timeline: Timeline = createTimeline()
+        const task = createEventTask(1, { event() {}, error() {}, end() {} })
+        const scheduledTask = createScheduledTask(100, 0, 0, task, createScheduler())
+
+        timeline.add(scheduledTask)
+
+        same(scheduledTask, timeline.readyTasks(200)[0])
+      }),
+    ]),
+  ]),
+
+  describe(`remove`, [
+    given(`a scheduled task`, [
+      it(`returns true if it can be removed`, ({ ok }) => {
+        const timeline: Timeline = createTimeline()
+        const task = createEventTask(1, { event() {}, error() {}, end() {} })
+        const scheduledTask = createScheduledTask(100, 0, 0, task, createScheduler())
+
+        timeline.add(scheduledTask)
+
+        ok(timeline.remove(scheduledTask))
+      }),
+
+      it(`returns false if it can not be remove`, ({ notOk }) => {
+        const timeline: Timeline = createTimeline()
+        const task = createEventTask(1, { event() {}, error() {}, end() {} })
+        const scheduledTask = createScheduledTask(100, 0, 0, task, createScheduler())
+
+        notOk(timeline.remove(scheduledTask))
+      }),
+    ]),
+  ]),
+
+  describe(`removeAll`, [
+    given(`a predicate (ScheduledTask -> boolean)`, [
+      it(`removes all that match the predicate`, ({ equal }) => {
+        const timeline: Timeline = createTimeline()
+        const task = createEventTask(1, { event() {}, error() {}, end() {} })
+        const scheduledTask = createScheduledTask(100, 0, 0, task, createScheduler())
+        const scheduledTaskTwo = createScheduledTask(200, 0, 0, task, createScheduler())
+
+        timeline.add(scheduledTask)
+        timeline.add(scheduledTaskTwo)
+
+        timeline.removeAll((scheduledTask: ScheduledTask) => scheduledTask.task === task)
+
+        equal(0, timeline.readyTasks(5000).length)
       }),
     ]),
   ]),
